0d210a108fa064ec29c1830844360c543b1ccc99
kamrul <kamrul444216@gmail.com>
2024-03-16 23:49:40 +0600

    Optimized Search Field

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -3,23 +3,15 @@ const prisma = new PrismaClient();
 
 const getAllAdminFromDB = async (params: any) => {
   const andConditions: Prisma.AdminWhereInput[] = [];
-
+  const fields = ["name", "email"];
   if (params.searchTerm) {
     andConditions.push({
-      OR: [
-        {
-          name: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
+      OR: fields.map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
         },
-        {
-          email: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
-        },
-      ],
+      })),
     });
   }
 
