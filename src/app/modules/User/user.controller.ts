509cfbd95eba6da1b702e405f40e3f82fa7ffa93
kamrul <kamrul444216@gmail.com>
2024-03-16 22:44:57 +0600

    Response Formatting And Exception Handling

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -2,9 +2,20 @@ import { Request, Response } from "express";
 import { userService } from "./user.service";
 
 const createAdmin = async (req: Request, res: Response) => {
-  console.log("controller data", req.body);
-  const result = await userService.createAdmin(req.body);
-  res.send(result);
+  try {
+    const result = await userService.createAdmin(req.body);
+    res.status(200).json({
+      success: true,
+      message: "Admin created successfully.",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error?.name || "Something went wrong.",
+      error: error,
+    });
+  }
 };
 
 export const userController = {
