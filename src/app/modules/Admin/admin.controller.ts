3c373ec6c4300bc938aca09d5726f3d0e0d21333
kamrul <kamrul444216@gmail.com>
2024-03-19 02:47:05 +0600

    Configure Routes in index file

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -3,8 +3,7 @@ import { AdminService } from "./admin.service";
 import pick from "../../../shared/pick";
 import { adminFilterableFields } from "./admin.constant";
 import sendResponse from "../../../shared/sendResponse";
-
-
+import httpStatus from "http-status";
 
 const getAllAdminFromDB = async (req: Request, res: Response) => {
   try {
@@ -12,7 +11,7 @@ const getAllAdminFromDB = async (req: Request, res: Response) => {
     const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
     const result = await AdminService.getAllAdminFromDB(filter, options);
     sendResponse(res, {
-      statusCode: 200,
+      statusCode: httpStatus.OK,
       success: true,
       message: "Admin data fetched.",
       meta: result.meta,
@@ -32,7 +31,7 @@ const getAdminByIdFromDB = async (req: Request, res: Response) => {
   try {
     const result = await AdminService.getAdminByIdFromDB(id);
     sendResponse(res, {
-      statusCode: 200,
+      statusCode: httpStatus.OK,
       success: true,
       message: "Admin data fetched.",
       data: result,
@@ -51,7 +50,7 @@ const updateAdminById = async (req: Request, res: Response) => {
   try {
     const result = await AdminService.updateAdminById(id, req.body);
     sendResponse(res, {
-      statusCode: 200,
+      statusCode: httpStatus.OK,
       success: true,
       message: "Admin data updated successfully.",
       data: result,
@@ -70,7 +69,7 @@ const deleteAdminFromDB = async (req: Request, res: Response) => {
   try {
     const result = await AdminService.deleteAdminFromDB(id);
     sendResponse(res, {
-      statusCode: 200,
+      statusCode: httpStatus.OK,
       success: true,
       message: "Admin deleted successfully.",
       data: result,
@@ -89,7 +88,7 @@ const softDeleteAdminFromDB = async (req: Request, res: Response) => {
   try {
     const result = await AdminService.softDeleteAdminFromDB(id);
     sendResponse(res, {
-      statusCode: 200,
+      statusCode: httpStatus.OK,
       success: true,
       message: "Admin soft deleted successfully.",
       data: result,
