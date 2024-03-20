e1e017eadaf6bdc8af3e818f95892a1758ed8bda
kamrul <kamrul444216@gmail.com>
2024-03-21 02:44:08 +0600

    generate access token using refresh token

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { prisma } from "../../../shared/prisma";
 import bcrypt from "bcrypt";
 import { jwtHelpers } from "./../../../helpers/jwtHelpers";
+import jwt from "jsonwebtoken";
 
 const loginUser = async (payload: { email: string; password: string }) => {
   const userData = await prisma.user.findUniqueOrThrow({
@@ -44,7 +45,38 @@ const loginUser = async (payload: { email: string; password: string }) => {
 };
 
 const generateRefreshToken = async (token: string) => {
-  console.log("refreshToken", {token});
+  let decodedData;
+  try {
+    decodedData = jwt.verify(token, "abcdefghij");
+    console.log(decodedData);
+  } catch (error) {
+    throw new Error("You are not authorized!");
+  }
+
+  const userData = await prisma.user.findFirst({
+    where: {
+      email: decodedData?.email,
+    },
+  });
+
+  if(!userData){
+    throw new Error("User not found!")
+  }
+
+  
+  const accessToken = jwtHelpers.generateToken(
+    {
+      email: userData.email,
+      role: userData.role,
+    },
+    "abcdefg",
+    "5m"
+  );
+
+  return {
+    accessToken,
+    needPasswordChange: userData.needPasswordChange,
+  };
 };
 
 export const AuthService = {
