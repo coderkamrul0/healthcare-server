3df6d684519da9779dda64733be36f8dbc934182
kamrul <kamrul444216@gmail.com>
2024-03-21 00:54:08 +0600

    change validation middleware filepath and organized code

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,32 +1,17 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import { AdminController } from "./admin.controller";
-import z, { AnyZodObject } from "zod";
+import validateRequest from "../../middleware/validateRequest";
+import { AdminValidationSchemas } from "./admin.validation";
 
 const router = express.Router();
 
-const update = z.object({
-  body: z.object({
-    name: z.string().optional(),
-    contactNumber: z.string().optional(),
-  }),
-});
-
-const validateRequest = (schema: AnyZodObject) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await schema.parseAsync({
-        body: req.body,
-      });
-      return next();
-    } catch (err) {
-      next(err);
-    }
-  };
-};
-
 router.get("/", AdminController.getAllAdminFromDB);
 router.get("/:id", AdminController.getAdminByIdFromDB);
-router.patch("/:id", validateRequest(update), AdminController.updateAdminById);
+router.patch(
+  "/:id",
+  validateRequest(AdminValidationSchemas.update),
+  AdminController.updateAdminById
+);
 router.delete("/:id", AdminController.deleteAdminFromDB);
 router.delete("/soft/:id", AdminController.softDeleteAdminFromDB);
 
