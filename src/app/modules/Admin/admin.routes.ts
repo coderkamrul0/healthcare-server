c3dbc1cca982707055cf02e63745100649c9a60f
kamrul <kamrul444216@gmail.com>
2024-03-22 23:44:19 +0600

    implementing role-based access control on api routes.

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -2,17 +2,36 @@ import express from "express";
 import { AdminController } from "./admin.controller";
 import validateRequest from "../../middleware/validateRequest";
 import { AdminValidationSchemas } from "./admin.validation";
+import auth from "../../middleware/auth";
+import { UserRole } from "@prisma/client";
 
 const router = express.Router();
 
-router.get("/", AdminController.getAllAdminFromDB);
-router.get("/:id", AdminController.getAdminByIdFromDB);
+router.get(
+  "/",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminController.getAllAdminFromDB
+);
+router.get(
+  "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminController.getAdminByIdFromDB
+);
 router.patch(
   "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
   validateRequest(AdminValidationSchemas.update),
   AdminController.updateAdminById
 );
-router.delete("/:id", AdminController.deleteAdminFromDB);
-router.delete("/soft/:id", AdminController.softDeleteAdminFromDB);
+router.delete(
+  "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminController.deleteAdminFromDB
+);
+router.delete(
+  "/soft/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminController.softDeleteAdminFromDB
+);
 
 export const AdminRoutes = router;
