2b1ef3b6a8579894a5898aa0ad76bd779c70ef81
kamrul <kamrul444216@gmail.com>
2024-04-04 23:01:43 +0600

    Get all patient and get single patient by id

---
+++ src/app/modules/Patient/patient.controller.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import catchAsync from "../../../shared/catchAsync";
+import pick from "../../../shared/pick";
+import { patientFilterableFields } from "./patient.constant";
+import { PatientService } from "./patient.service";
+import sendResponse from "../../../shared/sendResponse";
+import httpStatus from "http-status";
+
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, patientFilterableFields);
+  const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+
+  const result = await PatientService.getAllFromDB(filters, options);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Patient retrieval successfully",
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
+const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await PatientService.getByIdFromDB(id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Patient retrieval successfully",
+    data: result,
+  });
+});
+
+export const PatientController = {
+  getAllFromDB,
+  getByIdFromDB,
+};
--- src/app/modules/Patient/patient.routes.ts
+++ src/app/modules/Patient/patient.routes.ts
@@ -1,5 +1,10 @@
 import express from "express";
+import { PatientController } from "./patient.controller";
 
 const router = express.Router();
+
+router.get("/", PatientController.getAllFromDB);
+
+router.get("/:id", PatientController.getByIdFromDB);
 
 export const PatientRoutes = router;
