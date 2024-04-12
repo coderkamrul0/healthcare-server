9a317abd74cec777bcc26af760bdb478b60db748
kamrul <kamrul444216@gmail.com>
2024-04-13 01:52:17 +0600

    Create doctor schedule

---
+++ src/app/modules/DoctorSchedule/doctorSchedule.routes.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { DoctorScheduleController } from "./doctorSchedule.controller";
+import auth from "../../middleware/auth";
+import { UserRole } from "@prisma/client";
+
+const router = express.Router();
+
+router.post("/", auth(UserRole.DOCTOR), DoctorScheduleController.insertIntoDB);
+
+export const DoctorScheduleRoutes = router;
