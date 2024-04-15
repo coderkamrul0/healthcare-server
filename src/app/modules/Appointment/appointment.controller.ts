975798909492c07f4ae0604457eab5b93f5bbeab
kamrul <kamrul444216@gmail.com>
2024-04-15 22:49:41 +0600

    Retrieving my appointment base on patient and doctor

--- src/app/modules/Appointment/appointment.controller.ts
+++ src/app/modules/Appointment/appointment.controller.ts
@@ -4,6 +4,7 @@ import sendResponse from "../../../shared/sendResponse";
 import { AppointmentService } from "./appontment.service";
 import httpStatus from "http-status";
 import { IAuthUser } from "../../interfaces/common";
+import pick from "../../../shared/pick";
 
 const createAppointment = catchAsync(
   async (req: Request & { user?: IAuthUser }, res: Response) => {
@@ -22,6 +23,27 @@ const createAppointment = catchAsync(
   }
 );
 
+const getMyAppointment = catchAsync(
+  async (req: Request & { user?: IAuthUser }, res: Response) => {
+    const user = req.user;
+    const filters = pick(req.query, ["status", "paymentStatus"]);
+    const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+
+    const result = await AppointmentService.getMyAppointment(
+      user as IAuthUser,
+      filters,
+      options
+    );
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "My Appointment retrieve successfully",
+      data: result,
+    });
+  }
+);
+
 export const AppointmentController = {
   createAppointment,
+  getMyAppointment,
 };
